e89b25532f7972715178394988b547326ba7c5aa
greeksharifa <greeksharifa@gmail.com>
2024-07-08 16:33:23 +0900

    base dataset

---
+++ dataset/base_dataset.py
@@ -0,0 +1,58 @@
+import json
+from typing import Iterable
+import pandas as pd
+import torch
+
+from torch.utils.data import Dataset
+
+
+class BaseDataset(Dataset):
+    def __init__(self, args):
+        # self, vis_processor=None, text_processor=None, vis_root=None, ann_paths=[]):
+        """
+        vis_root (string): Root directory of images (e.g. coco/images/)
+        ann_root (string): directory to store the annotation file
+        """
+        vis_root=args.vis_root
+        vis_processor=args.vis_processor
+        text_processor=args.text_processor
+        ann_paths=args.ann_paths
+        
+        self.vis_root = vis_root
+        self.annotation = []
+        for ann_path in ann_paths:
+            if any(ext in ann_path for ext in ['csv', 'tsv']):
+                df = pd.read_csv(ann_path)
+                self.annotation.extend(df.to_dict(orient="records"))
+                
+            elif 'jsonl' in ann_path:
+                with open(ann_path, "r") as f:
+                    self.annotation.extend([json.loads(line) for line in f])
+
+            else:
+                with open(ann_path, "r") as f:
+                    loaded = json.load(f)
+                    if isinstance(loaded, list):
+                        self.annotation.extend(loaded)
+                    elif isinstance(loaded, dict):
+                       self.annotation.extend([{"sample_id": k, **v} if isinstance(v, dict) else {"sample_id": k, "data": v} for k, v in loaded.items()])
+
+
+        self.vis_processor = vis_processor
+        self.text_processor = text_processor
+
+        self._add_instance_ids()
+
+    def __len__(self):
+        return len(self.annotation)
+
+    def collater(self, samples):
+        raise NotImplementedError
+
+    def set_processors(self, vis_processor, text_processor):
+        self.vis_processor = vis_processor
+        self.text_processor = text_processor
+
+    def _add_instance_ids(self, key="instance_id"):
+        for idx, ann in enumerate(self.annotation):
+            ann[key] = str(idx)
