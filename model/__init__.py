40fef7446a848c8395325416080cf4e6e14a0070
greeksharifa <greeksharifa@gmail.com>
2025-02-21 19:55:47 +0900

    generate sub-qas

---
+++ model/__init__.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import List, Any
+
+import torch
+
+from config.configs import Config
+from model.models import *
+
+
+MODEL_REGISTRY = {
+    # "qwen2-vl-2b": Qwen2VL,
+    # "qwen2-vl-7b": Qwen2VL,
+
+    "qwen2.5-vl-7b": C2RFramework,
+    "qwen2.5-vl-3b": Qwen2_5VL,
+    
+    # "GPT4o": GPT4o,
+}
+
+
+def get_model(cfg: Config) -> C2RFramework:
+    model_name = cfg.model_cfg.model_name
+    if model_name not in MODEL_REGISTRY:
+        raise ValueError(f"Model {model_name} not found in registry")
+    
+    # model_id = cfg.model_cfg.model_id
+    model_class = MODEL_REGISTRY[model_name]
+    model = model_class(cfg)
+
+    return model
+
--- model/models.py
+++ model/models.py
@@ -11,7 +11,7 @@ import torch
 from transformers import AutoProcessor, AutoModelForCausalLM, AutoTokenizer
 from vllm import LLM, SamplingParams
 
-from configs.config import Config
+from config.configs import Config
 # from utils.logger import get_logger
 # from utils.util import transpose_list, get_confidence_and_ppl
 # from dataset.video_dataset import EXTs
@@ -28,29 +28,46 @@ class C2RFramework(ABC):
         model_id = self.cfg.model_cfg.model_id
         cache_dir = self.cfg.model_cfg.cache_dir
 
-        self.load_model_and_processor(model_id, cache_dir)
+        self.llm = self.load_model_and_processor(model_id, cache_dir)
 
         self.model.generation_config.do_sample=False
         self.model.generation_config.temperature=None
         self.model.generation_config.top_p=None
         self.model.generation_config.top_k=None
     
-    @abstractmethod
+    # @abstractmethod
     def load_model_and_processor(self, model_id: str, cache_dir: str):
-        pass
+        llm = LLM(
+            model=model_id, 
+            trust_remote_code=True, 
+            tensor_parallel_size=self.cfg.model_cfg.tensor_parallel_size, 
+            gpu_memory_utilization=self.cfg.model_cfg.gpu_memory_utilization, 
+            swap_space=self.cfg.model_cfg.swap_space,
+        )
+        return llm
+        # pass
 
-    @abstractmethod
-    def get_inputs(self, prompts: List[str], visions: Any, vpaths: List[Path] = None) -> Any:
-        """
-        Apply template to the text inputs and visions.
-        Args:
-            prompts: List[str]
-            visions: List[Any]
-            vpaths: List[str]
-        Returns: list of inputs for model
-            transformers.BatchFeature
-        """
-        pass
+    # @abstractmethod
+    # def get_inputs(self, prompts: List[str], visions: Any, vpaths: List[Path] = None) -> Any:
+    #     """
+    #     Apply template to the text inputs and visions.
+    #     Args:
+    #         prompts: List[str]
+    #         visions: List[Any]
+    #         vpaths: List[str]
+    #     Returns: list of inputs for model
+    #         transformers.BatchFeature
+    #     """
+    #     pass
+
+    def generate(self, prompts: List[Any]) -> List[Any]:
+        sampling_params = SamplingParams(
+            max_num_seqs=self.cfg.model_cfg.max_num_seqs,
+            max_new_tokens=self.cfg.model_cfg.max_new_tokens,
+            temperature=self.cfg.model_cfg.temperature,
+        )
+        outputs = self.llm.generate(prompts, sampling_params)
+        return outputs
 
     def _generate(self,
                   inputs: Any,
@@ -58,6 +75,7 @@ class C2RFramework(ABC):
                   max_new_tokens: int,
                   **kwargs
                   ) -> dict:
+        outputs = self.llm.generate(inputs, sampling_params)
         pass
 
     def postprocess_outputs(self, outputs: List[str]) -> List[str]:
@@ -94,3 +112,7 @@ class C2RFramework(ABC):
                                  ) -> Tuple[List[str], List[float], List[float], List[float], List[str]]:
         pass
     
+class Qwen2_5VL(C2RFramework):
+    def __init__(self, cfg: Config):
+        super().__init__(cfg)
+        # self.model = self.load_model_and_processor(cfg.model_cfg.model_id, cfg.model_cfg.cache_dir)
